e1a5bd7d4d058ce45fe74e36451ba5c982c2746d
OSyyds <3062927325@qq.com>
2023-09-22 16:56:43 +0800

    创建一个两个数字相加的例子

---
+++ C++_Primer/Chapter01/add.cpp
@@ -0,0 +1,19 @@
+/*************************************************************************
+    > File Name: add.cpp
+    > Author: 王小禹
+    > Mail: 3062927325@qq.com 
+    > Created Time: 2023年09月22日 星期五 16时52分09秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main () {
+	
+	cout << "Enter two numbers:" << endl;
+	int v1 = 0, v2 = 0;
+	cin >> v1 >> v2;
+	cout << "The sum of " << v1 << " and " << v2 << " is " << v1+v2 << endl;
+	
+	return 0;
+}
