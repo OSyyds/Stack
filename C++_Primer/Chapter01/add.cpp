d013f7fb12e0be7d95ca15c43fed441d6719dd4e
OSyyds <3062927325@qq.com>
2023-10-02 18:07:06 +0800

    用于学习注释一节

--- C++_Primer/Chapter01/add.cpp
+++ C++_Primer/Chapter01/add.cpp
@@ -8,11 +8,17 @@
 #include<iostream>
 using namespace std;
 
+/*
+ * 简单主函数:
+ * 读取两个数，求它们的和
+ */
+
 int main () {
 	
+	//提示用户输入两个数
 	cout << "Enter two numbers:" << endl;
-	int v1 = 0, v2 = 0;
-	cin >> v1 >> v2;
+	int v1 = 0, v2 = 0; //保存我们读入的输入数据的变量
+	cin >> v1 >> v2; //读取输入数据
 	cout << "The sum of " << v1 << " and " << v2 << " is " << v1+v2 << endl;
 	
 	return 0;
