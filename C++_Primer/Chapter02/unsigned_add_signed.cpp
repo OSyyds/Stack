4d3fa6ac06cb96df8b28f109d34afff3ad74e495
OSyyds <3062927325@qq.com>
2023-11-23 22:11:02 +0800

    有符号数和无符号数相加

---
+++ C++_Primer/Chapter02/unsigned_add_signed.cpp
@@ -0,0 +1,18 @@
+/*************************************************************************
+    > File Name: unsigned_add_signed.cpp
+    > Author: 王小禹
+    > Mail: 3062927325@qq.com 
+    > Created Time: Thu 23 Nov 2023 10:06:16 PM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main () {
+	unsigned int u = 10;
+	int i = -42;
+
+	cout << i + i << endl;
+	cout << u + i << endl;
+	return 0;
+}
